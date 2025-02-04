7f5c4dc70b9c04ec8e1d9da715ad0a0f4100839b
Till Simon <mail@tillsimon.com>
2025-02-05 00:23:00 +0100

    Update example, use interrupt

--- README.md
+++ README.md
@@ -2,18 +2,19 @@
 
 An AI agent [built with LangGraph](https://langchain-ai.github.io/langgraphjs/) integrating [gotoHuman](https://gotohuman.com) to keep a **human in the loop**.
 
-[gotoHuman](https://gotohuman.com) helps you build production-ready AI workflows by making it really easy to include human approvals. Keep a human in the loop to review AI‑generated content, approve critical actions or provide input.  
-Set up a fully customized review step capturing any relevant data (text, images, markdown,...) and required human input (buttons, checkboxes, inputs,...). Then trigger it from your application whenever human review from your team is needed.
+[gotoHuman](https://gotohuman.com) provides you with a central dashboard to review AI‑generated content, approve critical actions or provide input. And it seamlessly integrates with the AI stack of your choice.  
 
-This example workflow uses our JS/TS SDK to [send requests](https://docs.gotohuman.com/send-requests) for human review.
+This example workflow uses our [Typescript SDK](https://github.com/gotohuman/gotohuman-js-sdk).
 
-Trigger it with the email address of a new lead (from a waitlist, newsletter signup or lead magnet) to draft a personalized initial email outreach that is reviewed by a human before it is sent out.
+It takes the email address of a new sales lead and drafts a personalized initial email outreach. The draft is reviewed and revised by a human before it gets sent out.
 
 ## Set it up
 
 ### Create a review form
 
-This example uses our **"Sales Lead (Simple)"** template which contains just the right fields for our use case. Select it when [creating a new review form](https://app.gotohuman.com/create). For the webhook, enter the URL where you deploy this app. It is called for each review response to continue the workflow.  
+In gotoHuman, you can simply import the form template used here with the ID `OmmAnhbnWmird3oz60q2`.  
+For the webhook, enter the URL where you deploy this app. It is called for each review response to resume your graph.  
+
 Reviewers will find new pending reviews in their [gotoHuman inbox](https://app.gotohuman.com). You can also opt-in to receive a short-lived public link that you can freely send to reviewers.
 
 ### Deploy this agent
@@ -29,8 +30,12 @@ POSTGRES_CONN_STRING="postgres://..."
 
 ### Run it
 
-Trigger your agent whenever you get your hands on a new lead:
+Trigger your agent whenever you get your hands on a new email address:
 
+#### Manually in gotoHuman
+Create a new trigger form with a text input field with ID `email` and include the same webhook as above. You'll see a trigger button appear in gotoHuman.
+
+#### Via API
 `HTTP POST [DEPLOY_URL]/api/agent`
 ```json
 {
--- src/app/api/agent/route.ts
+++ src/app/api/agent/route.ts
@@ -2,11 +2,11 @@ export const maxDuration = 60;
 export const dynamic = 'force-dynamic';
 
 import { CheerioWebBaseLoader } from "@langchain/community/document_loaders/web/cheerio";
-import { AIMessage, BaseMessage, HumanMessage, SystemMessage, ToolMessage } from "@langchain/core/messages";
+import { AIMessage, BaseMessage, HumanMessage, SystemMessage } from "@langchain/core/messages";
 import { tool } from "@langchain/core/tools";
 import { z } from "zod";
 import { ChatOpenAI } from "@langchain/openai";
-import { StateGraph, START, END } from "@langchain/langgraph";
+import { StateGraph, START, END, Command, interrupt } from "@langchain/langgraph";
 import { Annotation } from "@langchain/langgraph";
 import { PostgresSaver } from "@langchain/langgraph-checkpoint-postgres";
 import { ToolNode } from "@langchain/langgraph/prebuilt";
@@ -23,7 +23,9 @@ export async function POST(request: Request) {
     messages: Annotation<BaseMessage[]>({
       reducer: (x, y) => x.concat(y),
     }),
+    emailAddress: Annotation<string>,
     leadWebsiteUrl: Annotation<string>,
+    emailToSend: Annotation<string>,
   })
   
   // Define the tools for the agent to use
@@ -68,7 +70,7 @@ export async function POST(request: Request) {
       You will write an email for ${senderName} who wants to reach out to a new prospect who left their email address: ${emailAddress} . ${senderName} works for the following company:
       ${senderCompanyDesc}
       Write no more than 300 words.
-      ${!noDomain ? 'It must be tailored as much as possible to the prospect\'s company based on the website information we fetched. Don\'t mention that we got the information from the website.' : ''}`),
+      ${!noDomain ? 'It must be tailored as much as possible to the prospect\'s company based on the website information we fetched. Don\'t mention that we got the information from the website. Include no placeholders! Your response should be nothing but the pure email body!' : ''}`),
       new HumanMessage((noDomain ? `No additional information found about the prospect` : `#Company website summary:
       ${companyDescription}`)),
     ];
@@ -84,66 +86,21 @@ export async function POST(request: Request) {
       companyDescription: z.string().describe("A description of the company based on the content found on its website."),
     }),
   });
-
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const askHumanTool = tool((_) => {
-    return "The human response will be injected";
-  }, {
-    name: "askHuman",
-    description: "Ask the human for review.",
-    schema: z.object({
-      emailAddress: z.string().describe("The email address of the new lead."),
-      websiteUrl: z.string().describe("The website url derived from the lead email address."),
-      companyDescription: z.string().describe("A description of the company based on the content found on its website."),
-      emailDraft: z.string().describe("A draft for a sales email that needs human review."),
-    }),
-  });
-
-  const sendEmailTool = tool(({emailRecipient, emailBody}) => {
-    // TODO: implement email sending.
-    // Again, note that you can set up your form at gotoHuman with any webhook URL, so that a submitted review can trigger any HTTP endpoint you like to proceed your workflow. It doesn't have to go back to this app.
-    return `The email was sent to ${emailRecipient}: ${emailBody.slice(0,50)}`;
-  }, {
-    name: "sendEmail",
-    description: "Send an email.",
-    schema: z.object({
-      emailRecipient: z.string().describe("The email address to send to."),
-      emailBody: z.string().describe("The email body to send."),
-    }),
-  });
   
-  const tools = [webScrapeTool, summarizerTool, draftTool, sendEmailTool];
+  const tools = [webScrapeTool, summarizerTool, draftTool];
   const toolNode = new ToolNode(tools);
   
-  const model = new ChatOpenAI({ temperature: 0.25, model: "gpt-4o-mini" }).bindTools([...tools, askHumanTool]);
+  const model = new ChatOpenAI({ temperature: 0.25, model: "gpt-4o-mini" }).bindTools(tools);
   
   // Define the function that determines whether to continue or not
-  // We can extract the state typing via `StateAnnotation.State`
-  async function shouldContinue(state: typeof StateAnnotation.State): Promise<"tools" | "askHuman" | typeof END> {
+  async function determineNextNode(state: typeof StateAnnotation.State): Promise<"tools" | "askHuman"> {
     const lastMessage = state.messages[state.messages.length - 1];
     const castLastMessage = lastMessage as AIMessage;
-    // If there is no function call, then we finish
+    // If there are no tool calls, then we go to next node for human approval
     if (castLastMessage && !castLastMessage.tool_calls?.length) {
-      return END;
-    }
-    console.log("shouldContinue toolCall", castLastMessage.tool_calls?.[0])
-    // If tool call is askHuman, we return that node
-    // Send a review request to gotoHuman
-    if (castLastMessage.tool_calls?.[0]?.name === "askHuman") {
-      console.log("--- ASKING HUMAN ---")
-      const args = castLastMessage.tool_calls?.[0]?.args
-      const reviewRequest = gotoHuman.createReview(process.env.GOTOHUMAN_FORM_ID)
-        .addFieldData("email", args?.emailAddress)
-        .addFieldData("emailDomain", {url: args?.websiteUrl || "", label: "Website checked"})
-        .addFieldData("websiteSummary", args?.companyDescription)
-        .addFieldData("emailDraft", args?.emailDraft)
-        .addMetaData("threadId", threadId)
-        // .assignToUsers(["jess@acme.org"])
-      const gotoHumanResponse = await reviewRequest.sendRequest()
-      console.log("gotoHumanResponse", gotoHumanResponse)
       return "askHuman";
     }
-    // Otherwise if it isn't, we continue with the action node
+    // Otherwise, we process the tool call
     return "tools";
   }
   
@@ -157,73 +114,86 @@ export async function POST(request: Request) {
     return { messages: [response] };
   }
 
-  async function extractDomainStep() {
-    const url = extractDomain(req.email)
-    console.log("extractDomainStep " + url)
-    return { leadWebsiteUrl: url, messages: [new HumanMessage(`We got the email address of a new lead: ${req.email}. ${url ? (`Scrape the website of its' domain: ${url} . Then use the summarizer tool to describe it.`) : ""} Then write an outreach email and finally pass all information and the draft to a human to review it. If the reviewer accepts it, send the email. Note that the reviewer might have edited it.`)] }
+  async function extractDomainNode(state: typeof StateAnnotation.State) {
+    const url = extractDomain(state.emailAddress)
+    console.log("extractDomainNode " + url)
+    return { leadWebsiteUrl: url, messages: [new HumanMessage(`We got the email address of a new lead: ${state.emailAddress}. ${url ? (`Scrape the website of its' domain: ${url} . Then use the summarizer tool to describe it.`) : ""} Then write an outreach email.`)] }
   }
 
-  // We define a fake node to ask the human
-  function askHuman(state: typeof StateAnnotation.State): Partial<typeof StateAnnotation.State> {
-    return state;
+  function humanReviewNode(state: typeof StateAnnotation.State): Command {
+    const emailDraft = state?.messages?.length ? state.messages[state.messages.length - 1].content : ""
+    const result = interrupt({
+      emailDraft: emailDraft,
+    });
+    const { response, reviewedEmail, comment } = result;
+  
+    if (response === "retry") {
+      return new Command({ goto: "agent", update: { messages: [{role: "human", content: "Please regenerate the email draft and consider the following: " + comment}] } });
+    } else if (response === "approve") {
+      return new Command({ goto: "sendEmail", update: { emailToSend: reviewedEmail } });
+    }
+    return new Command({ goto: END });
   }
+
+  function sendEmailNode(state: typeof StateAnnotation.State) {
+    console.log("sending email to " + state.emailAddress, state.emailToSend.slice(0,50))
+    // TODO: implement email sending.
+    return {};
+  };
   
   // Define a new graph
   const workflow = new StateGraph(StateAnnotation)
-    .addNode("domainStep", extractDomainStep)
+    .addNode("domainStep", extractDomainNode)
     .addNode("agent", callModel)
     .addNode("tools", toolNode)
-    .addNode("askHuman", askHuman)
+    .addNode("askHuman", humanReviewNode, {ends:["agent", "sendEmail", END]})
+    .addNode("sendEmail", sendEmailNode)
     .addEdge(START, "domainStep")
     .addEdge("domainStep", "agent")
-    .addConditionalEdges("agent", shouldContinue)
+    .addConditionalEdges("agent", determineNextNode, ["askHuman", "tools"])
     .addEdge("tools", "agent")
-    .addEdge("askHuman", "agent");
+    .addEdge("sendEmail", END);
   
   // Initialize DB to persist state of the conversation thread between graph runs
   const checkpointer = PostgresSaver.fromConnString(process.env.POSTGRES_CONN_STRING!);
-  if (!req.meta?.threadId) //if this is already a response we don't need to setup again
+  if (!req.meta?.threadId) //if this is already a response from gotoHuman we don't need to setup again
     await checkpointer.setup();
   
-  // Finally, we compile it!
-  // This compiles it into a LangChain Runnable.
-  // Note that we're (optionally) passing the memory when compiling the graph
-  const app = workflow.compile({ checkpointer, interruptBefore: ["askHuman"] });
-  const appConfig = { configurable: { thread_id: threadId } }
+  const graph = workflow.compile({ checkpointer });
+  const config = { configurable: { thread_id: threadId } }
 
-  // we were called again with the review response from gotoHuman
-  // NOTE: You can set up your form at gotoHuman with any webhook URL, so that a submitted review can trigger any HTTP endpoint you like to proceed your workflow. It doesn't have to go back to this app.
-  if (req.responseValues) {
-    const accepted = req.responseValues.emailApproval?.value === 'approve'
+  if (req.type === 'review') {
+    // we were called again with the review response from gotoHuman
+
+    const approval = req.responseValues.emailApproval?.value
     const emailText = req.responseValues.emailDraft?.value;
-    const emailTextChanged = req.responseValues.emailDraft?.wasEdited;
-    console.log(`GTH accepted ${accepted} emailTextChanged ${emailTextChanged} emailText ${emailText.slice(0,50)}`)
-    const currentState = await app.getState(appConfig);
-    const currentMessages = currentState?.values?.messages
-    console.log("currentState.values.messages", currentMessages)
-    if (!currentMessages) return new Response("Couldn't load persisted thread with ID " + req.meta?.threadId, { status: 404 })
-    const askHumanToolCallId = currentState.values.messages[currentState.values.messages.length - 1].tool_calls[0].id;
+    const retryComment = req.responseValues.retryComment?.value;
+    console.log(`GTH approval ${approval} emailText ${emailText.slice(0,50)} retryComment ${retryComment}`)
 
-    // We now create the tool call with the id and the response we want
-    const toolMessage = new ToolMessage({
-      tool_call_id: askHumanToolCallId,
-      content: `The human supervisor ${accepted ? 'accepted' : 'rejected'} the drafted email. ${accepted && emailTextChanged ? `The text was edited, so please send this edited text instead: ${emailText}` : ''}`
-    });
-    // We now update the state
-    // Notice that we are also specifying `asNode: "askHuman"`
-    // This will apply this update as this node,
-    // which will make it so that afterwards it continues as normal
-    await app.updateState(appConfig, { messages: [toolMessage] }, "askHuman");
-    
-    console.log("--- State after update ---")
-    console.log(await app.getState(appConfig));
+    await graph.invoke(new Command({ resume: { response: approval, reviewedEmail: emailText, comment: retryComment } }), config);
+  } else if (req.type === 'trigger' || req.email) {
+    // we were called by the gotoHuman trigger or by another request including an email
+    const email = req.email || req.responseValues?.email?.value || "";
+
+    const inputs = { emailAddress: email };
+    await graph.invoke(inputs, config);
   }
 
-  // Use the Runnable
-  const finalState = await app.invoke(req.responseValues ? null : {}, appConfig);
-  const finalMsg = finalState?.messages?.length ? finalState.messages[finalState.messages.length - 1] : ""
-  console.log(finalMsg.content);
-  return Response.json({"answer": finalMsg.content, "domain": finalState.leadWebsiteUrl})
+  const state = await graph.getState(config);
+  if (state.next.length > 0 && state.next[0] === "askHuman") {
+    const dataFromInterrupt = state.tasks?.[0]?.interrupts?.[0]?.value
+    const reviewRequest = gotoHuman.createReview(process.env.GOTOHUMAN_FORM_ID)
+      .addFieldData("email", state.values?.emailAddress || "")
+      .addFieldData("emailDomain", {url: state.values?.leadWebsiteUrl || "", label: "Website checked"})
+      .addFieldData("emailDraft", dataFromInterrupt?.emailDraft)
+      .addMetaData("threadId", threadId)
+      // .assignToUsers(["jess@acme.org"])
+    const gotoHumanResponse = await reviewRequest.sendRequest()
+    console.log("gotoHumanResponse", gotoHumanResponse)
+    return Response.json({message: "The email draft needs human review.", link: gotoHumanResponse.gthLink}, {status: 200})
+  }
+  console.log("graph ended")
+  return Response.json({message: "Graph ended"}, {status: 200})
 }
 
 function extractDomain(email: string) {
